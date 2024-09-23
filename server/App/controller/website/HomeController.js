428045fe7997aef5e7ddd1bb5a3ef575a307104e
PradeepRai218 <pradeep.rai@wscubetech.com>
2024-09-23 22:36:54 +0530

    Mail Work

--- server/App/controller/website/HomeController.js
+++ server/App/controller/website/HomeController.js
@@ -1,5 +1,6 @@
 const { ObjectId } = require("mongodb");
-const { dbConnection } = require("../../../dbConnection")
+const { dbConnection } = require("../../../dbConnection");
+const {  transporter } = require("../../../mailConfig");
 
 let studentInsert=async (req,res)=>{
     let {uname,email,phone}=req.body
@@ -12,6 +13,55 @@ let studentInsert=async (req,res)=>{
         let db=await dbConnection();
         let studentTable=await db.collection("student")
         let finalres=await studentTable.insertOne(obj)
+
+         const info = await transporter.sendMail({
+            from: '"Enqiry mail 👻" <pradeep.9997@gmail.com>', // sender address
+            to: "pradeep.rai@wscubetech.com,roshanchaurasia990@gmail.com", // list of receivers
+            subject: "Contact Enquiry Form", // Subject line
+            text: "Contact Enquiry Form", // plain text body
+            html:`<html>
+                    <head>
+                    </head>
+                    <body>
+                        <table border="1" cellpadding="10">
+                             <tr>
+                                <th>Name</th>
+                                <td>${uname}</td>
+                             </tr>
+                             <tr>
+                                <th>Email</th>
+                                <td>${email}</td>
+                             </tr>
+                             <tr>
+                                <th>Phone</th>
+                                <td>${phone}</td>
+                             </tr>
+                        </table>
+                    </body>
+            
+            </html>`, // html body
+          });
+        
+
+          const infouser = await transporter.sendMail({
+            from: '"Enqiry mail 👻" <pradeep.9997@gmail.com>', // sender address
+            to: email, // list of receivers
+            subject: "Thank You", // Subject line
+            text: "Contact Enquiry Form", // plain text body
+            html:`<html>
+                    <head>
+                    </head>
+                    <body>
+                        Thanks for Enquiry
+                        
+                    </body>
+            
+            </html>`, // html body
+          });
+
+       
+
+
         let apiobj={
             status:true,
             message:finalres
